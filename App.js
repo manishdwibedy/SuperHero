ed7bb102135d59db46299e6daae6c14d9133031b
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-12-25 21:08:34 -0800

    Fixing header

--- App.js
+++ App.js
@@ -36,19 +36,9 @@ export default class App extends Component<{}> {
         return (
             <Container>
                 <Header>
-                    <Left>
-                        <Button transparent>
-                            <Icon name='arrow-back' />
-                        </Button>
-                    </Left>
                     <Body>
                     <Title>Super Hero</Title>
                     </Body>
-                    <Right>
-                        <Button transparent>
-                            <Icon name='menu' />
-                        </Button>
-                    </Right>
                 </Header>
             </Container>
         );
