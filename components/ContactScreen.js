0ec460ddffc78894f113f4904fac121f8bd1ceaa
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-12-31 20:24:23 -0800

    working on importing contacts.

--- components/ContactScreen.js
+++ components/ContactScreen.js
@@ -26,9 +26,99 @@ var Contacts = require('react-native-contacts')
 // import 'expo';
 
 export default class ContactScreen extends Component<{}> {
-    checkContactStatus(){
+    constructor(props) {
+        super(props);
+        this.fetchAllContacts = this.fetchAllContacts.bind(this);
+        this.checkContactStatus = this.checkContactStatus.bind(this);
 
+        this.aa = this;
+        this.state = {
+            phoneAuth: 'Access pending'
+        };
     }
+
+    // fetchAllContacts  = (event) =>{
+    async fetchAllContacts(){
+        console.log('bbbb');
+        chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        // chars = ['a', 'b', 'c', 'd', 'e'];
+        other = chars;
+
+        results = [];
+        denied = false;
+        for (i = 0; i < 26; i++) {
+            filter = chars[i]
+            Contacts.getContactsMatchingString(filter, (err, contacts) => {
+                if(err === 'denied'){
+                    denied = true;
+                } else {
+
+                    results.push.apply(results, contacts);
+
+                }
+            })
+            // if (denied){
+            //     break;
+            // }
+            // for (j= 0; j < 26; j++) {
+            //     if (denied){
+            //         break;
+            //     }
+            //     filter = chars[i] + chars[j]
+            //     Contacts.getContactsMatchingString(filter, (err, contacts) => {
+            //         if(err === 'denied'){
+            //             denied = true;
+            //         } else {
+            //
+            //             results.push.apply(results, contacts);
+            //
+            //         }
+            //     })
+            // }
+        }
+
+    console.log(results);
+    };
+
+    checkContactStatus(){
+        Contacts.checkPermission( (err, permission) => {
+            // Contacts.PERMISSION_AUTHORIZED || Contacts.PERMISSION_UNDEFINED || Contacts.PERMISSION_DENIED
+            if(permission === 'undefined'){
+                Contacts.requestPermission( (err, permission) => {
+                    if (err){
+                        // this.component.setState({
+                        //     phoneAuth: 'Error occurred'
+                        // });
+                    }
+                    else{
+                        if(permission === 'authorized'){
+                            this.fetchAllContacts();
+                            this.setState({
+                                phoneAuth: 'Access granted'
+                            });
+                        }
+                        if(permission === 'denied'){
+                            this.setState({
+                                phoneAuth: 'Access denied'
+                            });
+                        }
+                    }
+                })
+            }
+            if(permission === 'authorized'){
+                this.fetchAllContacts();
+                this.setState({
+                    phoneAuth: 'Access granted'
+                });
+            }
+            if(permission === 'denied'){
+                this.setState({
+                    phoneAuth: 'Access denied'
+                });
+            }
+        })
+    }
+
     render() {
 
         const { navigate } = this.props.navigation;
@@ -58,9 +148,10 @@ export default class ContactScreen extends Component<{}> {
                 <Content>
                     <H2 style={styles.headers}>Phone Contacts</H2>
 
-                    <Button block danger style={styles.contactAccess}>
-
-                        <Text>Access granted</Text>
+                    <Button block danger style={styles.contactAccess} onPress={this.checkContactStatus}>
+                        <Text>
+                            {this.state.phoneAuth}
+                        </Text>
                         <Icon name='ios-checkmark' />
                     </Button>
 
